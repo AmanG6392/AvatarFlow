aa86f1e7450777647230434adc14040674672466
Aman <amanguptaa127@gmail.com>
2026-03-25 17:10:39 +0530

    setup the environment variables

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -10,8 +10,20 @@ export const env = createEnv({
     DATABASE_URL: z.string().url(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
-      .default("development"),
-  },
+      .default("development"),      
+      Modal_Key: z.string(),
+      Modal_Secret: z.string(),
+      
+      AWS_ACCESS_KEY_ID: z.string(),
+      AWS_SECRET_ACCESS_KEY_ID: z.string(),
+      AWS_REGION: z.string(),
+      S3_BUCKET_NAME: z.string(),
+      
+      TEXT_TO_SPEECH_ENDPOINT: z.string().url(),
+      PHOTO_TO_VIDEO_ENDPOINT: z.string().url(),
+      SEIVE_FILE_TO_S3_ENDPOINT: z.string().url(),
+
+  },  
 
   /**
    * Specify your client-side environment variables schema here. This way you can ensure the app
@@ -27,10 +39,21 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
-    NODE_ENV: process.env.NODE_ENV,
-    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
-  },
+  DATABASE_URL: process.env.DATABASE_URL,
+  NODE_ENV: process.env.NODE_ENV,
+
+  Modal_Key: process.env.Modal_Key,
+  Modal_Secret: process.env.Modal_Secret,
+
+  AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY_ID: process.env.AWS_SECRET_ACCESS_KEY_ID,
+  AWS_REGION: process.env.AWS_REGION,
+  S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
+
+  TEXT_TO_SPEECH_ENDPOINT: process.env.TEXT_TO_SPEECH_ENDPOINT,
+  PHOTO_TO_VIDEO_ENDPOINT: process.env.PHOTO_TO_VIDEO_ENDPOINT,
+  SEIVE_FILE_TO_S3_ENDPOINT: process.env.SEIVE_FILE_TO_S3_ENDPOINT,
+},
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
    * useful for Docker builds.
